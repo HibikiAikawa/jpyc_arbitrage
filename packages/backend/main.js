67c1ab66e1d8aec17d0a0b5934df554244ce7f87
HibikiAikawa <hibikingxfin@gmail.com>
2022-06-14 01:09:09 +0900

    expressの叩き台を追加

--- packages/backend/main.js
+++ packages/backend/main.js
@@ -5,6 +5,18 @@ const address = JSON.parse(fs.readFileSync('./address.json', 'utf8'));
 const Tx = require("ethereumjs-tx").Transaction;
 const ethers = require("ethers");
 
+const express = require("express");
+const app = express();
+
 // TODO infra API取得
 const provider = ethers.getDefaultProvider('matic');
 console.log(address.TOKEN.JPYC)
+
+const server = app.listen(3002, function(){
+    console.log("Node.js is listening to PORT:" + server.address().port);
+});
+
+
+app.get("/test", function(req, res, next){
+    res.send('hello world');
+});
