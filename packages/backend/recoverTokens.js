68b827c42f1398cef49009ff9d04c617b33bf1ea
HibikiAikawa <hibikingxfin@gmail.com>
2022-07-03 01:47:06 +0900

    トークン引き出し用のjsファイル作成

---
+++ packages/backend/recoverTokens.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+const address = JSON.parse(fs.readFileSync("./address.json", "utf8"));
+const arbFunc = require("./arbHandler");
+
+const main = async () => {
+    console.log('recover JPYC.')
+    await arbFunc.recoverTokens(address.TOKEN.JPYC.Address);
+    console.log('recover USDC.')
+    await arbFunc.recoverTokens(address.TOKEN.USDC.Address);
+};
+
+main();
